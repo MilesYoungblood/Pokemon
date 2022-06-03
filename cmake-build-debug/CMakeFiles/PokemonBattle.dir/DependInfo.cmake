c1fb27c8b9dbfbdff382e49e08a132aacaa9e5f0
MilesYoungblood <mlyoungblood@mail.smu.edu>
2022-06-03 16:37:03 -0500

    1.1

---
+++ cmake-build-debug/CMakeFiles/PokemonBattle.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Miles Youngblood/OneDrive/Documents/GitHub/PokemonBattle/Moves.cpp" "CMakeFiles/PokemonBattle.dir/Moves.cpp.obj" "gcc" "CMakeFiles/PokemonBattle.dir/Moves.cpp.obj.d"
+  "C:/Users/Miles Youngblood/OneDrive/Documents/GitHub/PokemonBattle/Pokemon.cpp" "CMakeFiles/PokemonBattle.dir/Pokemon.cpp.obj" "gcc" "CMakeFiles/PokemonBattle.dir/Pokemon.cpp.obj.d"
+  "C:/Users/Miles Youngblood/OneDrive/Documents/GitHub/PokemonBattle/main.cpp" "CMakeFiles/PokemonBattle.dir/main.cpp.obj" "gcc" "CMakeFiles/PokemonBattle.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/PokemonBattle.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PokemonBattle.dir/Moves.cpp.obj"
+  "CMakeFiles/PokemonBattle.dir/Moves.cpp.obj.d"
+  "CMakeFiles/PokemonBattle.dir/Pokemon.cpp.obj"
+  "CMakeFiles/PokemonBattle.dir/Pokemon.cpp.obj.d"
+  "CMakeFiles/PokemonBattle.dir/main.cpp.obj"
+  "CMakeFiles/PokemonBattle.dir/main.cpp.obj.d"
+  "PokemonBattle.exe"
+  "PokemonBattle.exe.manifest"
+  "PokemonBattle.pdb"
+  "libPokemonBattle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PokemonBattle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
